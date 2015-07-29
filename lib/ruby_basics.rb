7398605121581c28e892408fa9f59ba0e1790bc2
rolandobrown <rolando.brown@gmail.com>
2015-07-29 15:59:58 -0400

    all done. comment question about specific error message

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,29 @@
 def division(num1, num2)
- 
+  num1 / num2 
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  name = name
 end
 
-def argue
-  
+def argue(phrase)
+  phrase = "I'm right and you are wrong!"
 end
 
-def greeting
-
+def greeting (greeting, name)
+  greeting = "Hi there, "
+  name = "Bobby!"
+  # Is there a way to create a specific error message?
 end
 
-def return_a_value
-
+def return_a_value (phrase = "Nice")
+  phrase 
 end
 
-def last_evaluated_value
-
+def last_evaluated_value (phrase = "expert")
+  phrase
 end
 
-def pizza_party
-
+def pizza_party (cheese= "cheese")
+  cheese
 end
